ed0a2a99846e3da6b283eb9b48811b02a19f6258
Denis Shneider <d_shneider@BY1-WW-486>
2024-10-10 17:14:20 +0300

    init: helmet-project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <body className='App'>
+      <header>
+          <h1>Добро пожаловать на наш сайт, посвященный шлемам!</h1>
+      </header>
+      <main className='App-header'>
+          <section>
+              <h2>Наши шлемы</h2>
+              <p>Качественные шлемы для велосипедистов, мотоциклистов, лыжников и сноубордистов. Максимальная защита и комфорт. Стильный дизайн и современные стандарты безопасности.</p>
+          </section>
+      </main>
+      <footer>
+          <p>&copy; 2024 Все права защищены.</p>
+      </footer>
+    </body>
+  );
+}
+
+export default App;
